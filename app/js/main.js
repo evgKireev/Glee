c03ae0334c32e506eab4b720883f349c3d531811
evgKireev <bob1_k@mail.ru>
2022-03-08 13:26:08 +0300

    shop1

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,30 @@
 $(function() {
+
+  $('.filter-recent__star').rateYo({
+    starWidth: "11px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    readOnly: true,
+    spacing: "7px"
+  });
+
+
+
+  $(".filter-price__input").ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    onStart: function(data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function(data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+  });
+
+
+
   $('.slider-top__inner').slick({
     arrows: false,
     dots: true,
