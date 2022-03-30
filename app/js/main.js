a9781ee7a5503391688775a22b01018fb3252a7a
evgKireev <bob1_k@mail.ru>
2022-03-31 00:03:11 +0300

    one-product

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,55 @@
 $(function() {
 
+  $('.related__inner').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: true,
+    nextArrow: $('.related__button-next'),
+    prevArrow: $('.related__button-prev'),
+
+
+  });
+
+
+
+
+  $('.one-product-tabs__link').on('click', function(e) {
+    e.preventDefault();
+
+    $('.one-product-tabs__link').removeClass('one-product-tabs__link--active');
+    $(this).addClass('one-product-tabs__link--active');
+    $('.one-product-tabs__content-item').removeClass('one-product-tabs__content-item--active');
+    $($(this).attr('href')).addClass('one-product-tabs__content-item--active');
+  });
+
+
+
+
+  $('.one-product__input').styler();
+
+  $('.one-product__img-big').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.product-slide__img-smoll'
+
+  });
+
+  $('.product-slide__img-smoll').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: '.one-product__img-big',
+    centerMode: true,
+    focusOnSelect: true,
+    vertical: true,
+
+  });
+
+
+
   $('.filter-recent__star').rateYo({
     starWidth: "11px",
     normalFill: "#d6d6d6",
